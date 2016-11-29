7d4eab0ebf35318a1a69e4a3acd69b67aa4963f1
martin <martin.hinz@ufg.uni-kiel.de>
2016-11-29 14:00:48 +0100

    Altered S3 method related documentation (and partly code) to ensure package checks are successful.

--- R/output_functions.R
+++ R/output_functions.R
@@ -3,6 +3,7 @@
 #' Checks if a variable is of class mortaar_life_table_list
 #'
 #' @param x a variable
+#' @param ... further arguments passed to or from other methods.
 #'
 #' @return true if x is a mortaar_life_table_list, false otherwise
 #'
@@ -18,7 +19,8 @@ is.mortaar_life_table_list <- function(x, ...) {"mortaar_life_table_list" %in% c
 #'
 #' Format an mortaar_life_table_list for pretty printing.
 #'
-#' @param life_table_list a mortaar_life_table_list
+#' @param x a mortaar_life_table_list
+#' @param ... further arguments passed to or from other methods.
 #'
 #' @return A string representation of the mortaar_life_table_list. Each format.mortaar_life_table
 #' is formated by its own method and concatenated. The names of the elements are used to specify
@@ -27,14 +29,14 @@ is.mortaar_life_table_list <- function(x, ...) {"mortaar_life_table_list" %in% c
 #' @examples
 #'
 #' test <- data.frame(male = round(runif(70)*100, 0), female = round(runif(70)*100, 0))
-#' format.mortaar_life_table_list(life.table(test))
+#' format(life.table(test))
 #'
 #' @export
-format.mortaar_life_table_list <- function(life_table_list, ...) {
+format.mortaar_life_table_list <- function(x, ...) {
   return_value <- ""
-  list_names <- names(life_table_list)
-  for (i in 1:length(life_table_list)) {
-    this_return_value <- format.mortaar_life_table(life_table_list[[i]], class_of_deceased = list_names[i], ...)
+  list_names <- names(x)
+  for (i in 1:length(x)) {
+    this_return_value <- format.mortaar_life_table(x[[i]], class_of_deceased = list_names[i], ...)
     return_value <- paste(return_value, this_return_value,sep="\n",collapse="\n")
   }
   invisible(return_value)
@@ -45,11 +47,12 @@ format.mortaar_life_table_list <- function(life_table_list, ...) {
 #' Print a mortaar_life_table_list according to the format.mortaar_life_table_list
 #'
 #' @param x a mortaar_life_table_list
+#' @param ... further arguments passed to or from other methods.
 #'
 #' @examples
 #'
 #' test <- data.frame(male = round(runif(70)*100, 0), female = round(runif(70)*100, 0))
-#' print.mortaar_life_table_list(life.table(test))
+#' print(life.table(test))
 #'
 #' @export
 print.mortaar_life_table_list <- function(x, ...) cat(format(x, ...), "\n")
@@ -59,6 +62,7 @@ print.mortaar_life_table_list <- function(x, ...) cat(format(x, ...), "\n")
 #' Checks if a variable is of class mortaar_life_table
 #'
 #' @param x a variable
+#' @param ... further arguments passed to or from other methods.
 #'
 #' @return true if x is a mortaar_life_table, false otherwise
 #'
@@ -74,34 +78,36 @@ is.mortaar_life_table <- function(x, ...) {"mortaar_life_table" %in% class(x)}
 #'
 #' Format an mortaar_life_table for pretty printing.
 #'
-#' @param life_table_list a mortaar_life_table
+#' @param x a mortaar_life_table
 #' @param class_of_deceased optional specify the class of deceased (male, female, undetermined, ...)
+#' @param ... further arguments passed to or from other methods.
 #'
 #' @return A string representation of the mortaar_life_table
 #'
 #' @examples
 #'
 #' test <- data.frame(male = round(runif(70)*100, 0), female = round(runif(70)*100, 0))
-#' format.mortaar_life_table(life.table(test)$male)
+#' format(life.table(test)$male)
 #'
+#' @importFrom utils capture.output
 #' @export
-format.mortaar_life_table <- function(life_table, class_of_deceased = NULL, ...)
+format.mortaar_life_table <- function(x, class_of_deceased = NULL, ...)
 {
   out_str <- list()
   class_of_deceased_str <- ""
   if (!is.null(class_of_deceased)) {
     class_of_deceased_str <- paste(" for", class_of_deceased)
   }
-  out_str$header <- paste("\n","\t Mortaar life table", class_of_deceased_str," of ",life_table$nSx[1]," individuals",sep = "")
+  out_str$header <- paste("\n","\t Mortaar life table", class_of_deceased_str," of ",x$nSx[1]," individuals",sep = "")
 
-  out_str$e0 <- paste("\n","Life expectation at birth (e0): ",round(life_table$ex[1],3), sep = "")
+  out_str$e0 <- paste("\n","Life expectation at birth (e0): ",round(x$ex[1],3), sep = "")
   age_class_names <- vector()
-  for (i in 1:length(life_table$x))  {
-    this_x <- life_table$x[i]
-    if (i == length(life_table$x)) {
+  for (i in 1:length(x$x))  {
+    this_x <- x$x[i]
+    if (i == length(x$x)) {
       this_next_x <- Inf
     } else {
-      this_next_x <- life_table$x[i+1]
+      this_next_x <- x$x[i+1]
     }
 
     this_age_class_name <- paste(this_x, this_next_x, sep="-")
@@ -110,16 +116,16 @@ format.mortaar_life_table <- function(life_table, class_of_deceased = NULL, ...)
 
   out_table <- data.frame(
     age.class = age_class_names,
-    nDx = round(life_table$nDx,3),
-    nSx = round(life_table$nSx,3),
-    nax = round(life_table$nax,3),
-    nqx = round(life_table$nqx,3),
-    lx = round(life_table$lx,3),
-    ndx = round(life_table$ndx,3),
-    nLx = round(life_table$nLx,3),
-    Tx = round(life_table$Tx,3),
-    ex = round(life_table$ex,3),
-    rel_bev = round(life_table$rel_bev,3)
+    nDx = round(x$nDx,3),
+    nSx = round(x$nSx,3),
+    nax = round(x$nax,3),
+    nqx = round(x$nqx,3),
+    lx = round(x$lx,3),
+    ndx = round(x$ndx,3),
+    nLx = round(x$nLx,3),
+    Tx = round(x$Tx,3),
+    ex = round(x$ex,3),
+    rel_bev = round(x$rel_bev,3)
     )
 
   return_value <- paste(out_str,collapse = "\n",sep="")
@@ -134,11 +140,12 @@ format.mortaar_life_table <- function(life_table, class_of_deceased = NULL, ...)
 #' Print a mortaar_life_table according to the format.mortaar_life_table
 #'
 #' @param x a mortaar_life_table
+#' @param ... further arguments passed to or from other methods.
 #'
 #' @examples
 #'
 #' test <- data.frame(male = round(runif(70)*100, 0), female = round(runif(70)*100, 0))
-#' print.mortaar_life_table(life.table(test)$male)
+#' print(life.table(test)$male)
 #'
 #' @export
 print.mortaar_life_table <- function(x, ...) cat(format(x, ...), "\n")
