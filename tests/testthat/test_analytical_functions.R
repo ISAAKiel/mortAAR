26533ce23f29b89ff8138fee108f2323d1506ef0
Martin Hinz <martin.hinz@ufg.uni-kiel.de>
2016-12-13 10:34:47 +0100

    added test for fetching non numeric colums error

--- tests/testthat/test_analytical_functions.R
+++ tests/testthat/test_analytical_functions.R
@@ -4,3 +4,9 @@ test_that("a mortaar_life_table_list is only produced from a data.frame", {
   expect_error(life.table(an_input_dataset), NA)
   expect_error(life.table(c(1,2)), "The input data is not a data.frame")
 })
+
+test_that("life.table fetches non numeric colums", {
+  a_wrong_input_dataset <- data.frame(male = rep(c("alt", "jung"),10))
+  expect_error(life.table(a_wrong_input_dataset),"The input data.frame has non-numeric columns. The following column is not numeric: male")
+  expect_error(life.table(an_input_dataset),NA)
+})
