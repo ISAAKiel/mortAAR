e97c0dfdb830cea6f9920428071a3cb52d633d76
nevrome <clemens@nevrome.de>
2017-03-10 13:01:01 +0100

    I somehow managed to overwrite Christophs changes from 89c001a438934c8facafd45eef8f3f304093318f and 202494f94bf6d0ed2ca29259b80b1197b7f50b10. No idea how. Should be fixed now.

--- R/analytical_functions.R
+++ R/analytical_functions.R
@@ -1,9 +1,14 @@
 #' life table
 #'
-#' \code{life.table} returns a list of life table(s) for a list of dataframe(s) with deseased (Dx) per age interval (x)
+#' \code{life.table} returns a list of life table(s) for a list of dataframe(s) with
+#' deseased (Dx) per age interval (x)
 #'
-#' @param neclist list or data.frame
-#' @param acv Age correction value, mean value of the age interval for the calculation of lx.
+#' @param neclist list of dataframes or single dataframe with columns 'x', 'a', 'Dx'
+#' @param acv vector, optional. Age correction values to determin the centre of the
+#' age interval for the calculation of L(x). Given values replace the standard values
+#' from the first age interval onwards. Standard values are:
+#' if x<5 then acv(x) = a(x) * 1/3 else acv(x) = a(x) * 1/2.
+#' Mainly used to correct higher mortality rates for infants.
 #'
 #' @details
 #' simple lifetable using Keyfitz and Flieger separation factors and
@@ -24,8 +29,8 @@
 #'   \item \bold{Lx} average years per person lived within x : \eqn{L(x) = a * (l(x) + l(x+1)) / 2}
 #'   \item \bold{Tx} sum of average years lived within current and remaining x : \eqn{T(x) = \Sigma L(x)}
 #'   \item \bold{ex} average years of life remaining (average life expectancy at mean(x)) : \eqn{e(x) = T(x) / l(x)}
-#'   \item \bold{Jx} gelebte Jahresanteile (to be translated)
-#'   \item \bold{Ax} Anteil an der Lebenspyramide
+#'   \item \bold{Jx} sum of remaining years at the mean of age interval x : \eqn{\sum D(x)*a(x) - (\sum D(1 - x-1)*a(1 - x-1) + D(x)/2 * a(x))}
+#'   \item \bold{Ax} percentage of J(x) of the sum of J(x) :  \eqn{J(x) * 100 / \Sigma J(x)}
 #' }
 #'
 #'
@@ -53,6 +58,7 @@
 #' )
 #'
 #' life.table(neclist)
+#' life.table(neclist, c(0.25,1/3,0.5))
 #'
 #' @importFrom magrittr "%>%"
 #'
@@ -187,10 +193,16 @@ life.table.df <- function(necdf, acv = c()) {
   # ex: average years of life remaining
   necdf['ex'] <- necdf['Tx'] / necdf['lx']
 
-  # Jx: gelebte Jahresanteile
-  necdf[1, 'Jx'] <- sum(necdf$Dx) - (necdf[1, 'Dx'] / 2)
+  # # Jx: gelebte Jahresanteile !Nein, Individuen und noch lebende !
+  #  necdf[1, 'Jx'] <- sum(necdf$Dx) - (necdf[1, 'Dx'] / 2)
+  #  for(i in 2:nrow(necdf)){
+  #    necdf[i, 'Jx'] <- sum(necdf$Dx) - ((necdf[i, 'Dx'] / 2) + sum(necdf[1:i-1,'Dx']))
+  #  }
+
+  # Jx: noch zu lebende Jahre
+  necdf[1, 'Jx'] <- sum(necdf[, 'Dx'] * necdf[, 'a']) - (necdf[1, 'Dx'] * multvec[1])
   for(i in 2:nrow(necdf)){
-    necdf[i, 'Jx'] <- sum(necdf$Dx) - ((necdf[i, 'Dx'] / 2) + sum(necdf[1:i-1,'Dx']))
+    necdf[i, 'Jx'] <- sum(necdf[, 'Dx'] * necdf[, 'a']) - (necdf[i, 'Dx'] * (multvec[i]) + sum(necdf[1:i-1,'Dx'] * necdf[1:i-1,'a']))
   }
 
   ## Ax: Anteil an der Lebenspyramide
