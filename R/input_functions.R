47e36dad109f346bbb3b2d9634bb98a4fcbdbed6
chrinne <crinne@ufg.uni-kiel.de>
2017-03-16 17:57:50 +0100

    Writing documentation for input functions

--- R/input_functions.R
+++ R/input_functions.R
@@ -1,22 +1,45 @@
-
-#This function resolves a field (RangeOfAgeFieldName) with the format "1-12" to two fields with the numeric values 1 and 12
-function1=function(dataframe,RangeOfAgeFieldName){
-  asd=dataframe
+#' Resolves a field (RangeOfAgeFieldName) with the format "1-12"
+#'
+#' Resolves a field (RangeOfAgeFieldName) with the format "1-12" to two fields with the numeric values 1 and 12
+#'
+#' @param x a dataframe
+#' @param RangeOfAgeFieldName Column name of the age class (as character)
+#'
+#' @return dataframe with additional columns "from" and "to"
+#'
+#' @examples
+#'
+#' #test=read.table("data-raw/siedl.txt",header=T,sep="\t")
+#' #test1=function1(test,"Jahrefeld")
+#'
+#' @export
+function1=function(x,RangeOfAgeFieldName){
+  asd=x
   names(asd)[which(names(asd)==RangeOfAgeFieldName)]="Range"
-  asd$beg=NA
-  asd$ende=NA
-  
+  asd$from=NA
+  asd$to=NA
+
   for(i in seq_along(asd$Range)){
-    asd$beg[i]=as.numeric(strsplit(as.character(asd$Range[i]),"-")[[1]][1])
-    asd$ende[i]=as.numeric(strsplit(as.character(asd$Range[i]),"-")[[1]][2])
+    asd$from[i]=as.numeric(strsplit(as.character(asd$Range[i]),"-")[[1]][1])
+    asd$to[i]=as.numeric(strsplit(as.character(asd$Range[i]),"-")[[1]][2])
   }
   names(asd)[which(names(asd)=="Range")]=RangeOfAgeFieldName
   return(asd)
 }
 
-#asd2=function1(alter,"Ausdr2"); head(asd2)
 
-#This function uses the adition function "+" to combine two variables setting NA values to zero; this is neccesary since the sum function (which could use na.rm=TRUE) sums up the values of a vektors input
+#' Uses the adition function "+" to combine two vectors setting NA values of the first vector to zero
+#'
+#' Uses the adition function "+" to combine two vectors setting NA values of the first vector to zero
+#'
+#' @param x a vector
+#' @param y vector to combine with
+#'
+#' @return dataframe with additional columns "from" and "to"
+#'
+#' @examples
+#'
+#' #summentest=summe(c(1,2,NA,4),c(5,6,7,8)
 summe=function(x,y){
   if(any(is.na(x))){
     x[is.na(x)]=0
@@ -25,17 +48,35 @@ summe=function(x,y){
 }
 
 
-#This function takes the count of funerals for a range of ages and produces a with the mean deaths per age (same as function2) / thereby it is possible to select a dependency on the Group
-function3=function(dataframe,CountOfFuneralsFieldName="NA",BeginOfAgeFieldName,EndOfAgeFieldName,GroupName="NA",methode="NA"){
-  asd=dataframe
-  if(CountOfFuneralsFieldName=="NA"){
+#' Creates the input for the function life.table
+#'
+#' Creates the input for the function life.table
+#'
+#' @param x a dataframe
+#' @param CountOfDeceasedFieldName Column name of the count of deceased (as character)
+#' @param BeginOfAgeFieldName Column name of the from field (as character)
+#' @param EndOfAgeFieldName Column name of the to field (as character)
+#' @param GroupName Column name of the grouping field (as character)
+#' @param methode name of the age class determination (as character) Options: "Standard" (default) (1,4,5,5,...), "Equal5" (5,5,...)
+#'
+#' @return list as input parameter for the function life.table
+#'
+#' @examples
+#'
+#' #test2=function3(x=test1,CountOfDeceasedFieldName="NA",BeginOfAgeFieldName="from",
+#' #EndOfAgeFieldName="to",GroupName="Geschlecht", methode="Standard")
+#'
+#' @export
+function3=function(x,CountOfDeceasedFieldName="NA",BeginOfAgeFieldName,EndOfAgeFieldName,GroupName="NA",methode="NA"){
+  asd=x
+  if(CountOfDeceasedFieldName=="NA"){
     asd$cof=rep(1,dim(asd)[1])
   }else{
-  names(asd)[which(names(asd)==CountOfFuneralsFieldName)]="cof"
+    names(asd)[which(names(asd)==CountOfDeceasedFieldName)]="cof"
   }
   names(asd)[which(names(asd)==BeginOfAgeFieldName)]="beg"
   names(asd)[which(names(asd)==EndOfAgeFieldName)]="ende"
-  
+
   ## Choosing which mnethod should be applied
   if(is.character(methode)){
     if(methode=="Standard"){
@@ -58,17 +99,17 @@ function3=function(dataframe,CountOfFuneralsFieldName="NA",BeginOfAgeFieldName,E
       meth=methode
     }
   }
-  
+
   ## Using Group Argument to subset data into several groups
   if(GroupName!="NA"){
     names(asd)[which(names(asd)==GroupName)]="Group"
-    
+
     remat=matrix(data=0,ncol=length(unique(asd$Group))+2,nrow=(max(asd$ende)+1))
     restab=as.data.frame(remat)
     names(restab)=c("Alter",as.character(unique(asd$Group)),"All")
     restab$Alter=seq(0,max(asd$ende),1)
-    
-    
+
+
     for(k in 1:length(unique(asd$Group))){
       for(i in which(asd$Group==unique(asd$Group)[k])){
         restab[is.element(restab$Alter,seq(asd$beg[i],asd$ende[i],1)),(k+1)]=summe(x=(restab[is.element(restab$Alter,seq(asd$beg[i],asd$ende[i],1)),(k+1)]),y=(asd$cof[i]/(length(seq(asd$beg[i],asd$ende[i],1)))))
@@ -78,39 +119,32 @@ function3=function(dataframe,CountOfFuneralsFieldName="NA",BeginOfAgeFieldName,E
       restab$All[is.element(restab$Alter,seq(asd$beg[i],asd$ende[i],1))]=summe(x=(restab$All[is.element(restab$Alter,seq(asd$beg[i],asd$ende[i],1))]),y=(asd$cof[i]/(length(seq(asd$beg[i],asd$ende[i],1)))))
     }
   }else{
-    
+
     restab=data.frame(Alter=seq(0,99,1),Bestattungen=0)
-    
+
     for(i in seq_along(asd[,1])){
       restab$Bestattungen[is.element(restab$Alter,seq(asd$beg[i],asd$ende[i],1))]=summe(x=(restab$Bestattungen[is.element(restab$Alter,seq(asd$beg[i],asd$ende[i],1))]),y=(asd$cof[i]/(length(seq(asd$beg[i],asd$ende[i],1)))))
     }
   }
-  
+
   for(u in seq_along(meth)){
     if(u==1){
       output1=as.data.frame(t(colSums(restab[c(1:meth[u]),],na.rm =TRUE)))
     }else{
-    v=u-1
-    output1=rbind(output1,as.data.frame(t(colSums(restab[(c(cumsum(meth)[v]+1):cumsum(meth)[u]),],na.rm =TRUE))))
+      v=u-1
+      output1=rbind(output1,as.data.frame(t(colSums(restab[(c(cumsum(meth)[v]+1):cumsum(meth)[u]),],na.rm =TRUE))))
     }
   }
   output1$Alter=cumsum(meth)-meth
-  output1$a=meth       
-  
+  output1$a=meth
+
   output=list(test=output1[,c("Alter","a",colnames(output1)[2])])
-   names(output[[1]])[3]="Dx"
+  names(output[[1]])[3]="Dx"
   for(u in 2:(dim(output1)[2]-2)){
-   output[[u]]=output1[,c("Alter","a",colnames(output1)[u+1])]
+    output[[u]]=output1[,c("Alter","a",colnames(output1)[u+1])]
     names(output[[u]])[3]="Dx"
   }
   names(output)=colnames(output1)[c(-1,-length(colnames(output1)))]
-  
+
   return(output)
 }
-
-#asd4=function3(dataframe=asd2,CountOfFuneralsFieldName="Anzahl.von.Individuum_nr",BeginOfAgeFieldName="beg",EndOfAgeFieldName="ende",GroupName="Geschlecht_kombiniert", methode="Standart")
-#asd4
-#dummy
-
-#standard=c(1,4,5,5,5,5,5,5)
-#hermann=c(5,5,5,5,5,5)
