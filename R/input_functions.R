362dba4ba02cbc7a6db195f9f490880fa8245104
Wolfgang Hamer <hamer@geographie.uni-kiel.de>
2017-06-12 13:50:13 +0200

    "Alter" has been changed to "Age" and the error producing the NA data.frame in the result list has been removed

--- R/input_functions.R
+++ R/input_functions.R
@@ -99,28 +99,28 @@ prep.life.table=function(x,dec="NA",agebeg,ageend,grnam="NA",methode="NA", age.r
     remat=matrix(data=0,ncol=length(unique(asd$Group))+2,nrow=(max(asd$ende,na.rm=T)+1))
     restab=as.data.frame(remat)
     # Set the dataframes column names to age, the groups names and all
-    names(restab)=c("Alter",as.character(unique(asd$Group)),"All")
+    names(restab)=c("Age",as.character(unique(asd$Group)),"All")
     # Set the age values to 0 to the maximum age +1
-    restab$Alter=seq(0,max(asd$ende,na.rm=T),1)
+    restab$Age=seq(0,max(asd$ende,na.rm=T),1)
 
     # For each Group (k) the deaths per age class (available years i) are summed up equally seperated by ages
     for(k in 1:length(unique(asd$Group))){
       for(i in which(asd$Group==unique(asd$Group)[k])){
-        restab[is.element(restab$Alter,seq(asd$beg[i],asd$ende[i],1)),(k+1)]=summe(x=(restab[is.element(restab$Alter,seq(asd$beg[i],asd$ende[i],1)),(k+1)]),y=(asd$cof[i]/(length(seq(asd$beg[i],asd$ende[i],1)))))
+        restab[is.element(restab$Age,seq(asd$beg[i],asd$ende[i],1)),(k+1)]=summe(x=(restab[is.element(restab$Age,seq(asd$beg[i],asd$ende[i],1)),(k+1)]),y=(asd$cof[i]/(length(seq(asd$beg[i],asd$ende[i],1)))))
       }
     }
     # Also for all Groups all deceased are summed up seperated on the according years
     for(i in seq_along(asd[,1])){
-      restab$All[is.element(restab$Alter,seq(asd$beg[i],asd$ende[i],1))]=summe(x=(restab$All[is.element(restab$Alter,seq(asd$beg[i],asd$ende[i],1))]),y=(asd$cof[i]/(length(seq(asd$beg[i],asd$ende[i],1)))))
+      restab$All[is.element(restab$Age,seq(asd$beg[i],asd$ende[i],1))]=summe(x=(restab$All[is.element(restab$Age,seq(asd$beg[i],asd$ende[i],1))]),y=(asd$cof[i]/(length(seq(asd$beg[i],asd$ende[i],1)))))
     }
 
   # If no groups (male, female, whatever, ...) are specified, do the same without considering groups
   }else{
 
-    restab=data.frame(Alter=seq(0,99,1),Bestattungen=0)
+    restab=data.frame(Age=seq(0,99,1),Deceased=0)
 
     for(i in seq_along(asd[,1])){
-      restab$Bestattungen[is.element(restab$Alter,seq(asd$beg[i],asd$ende[i],1))]=summe(x=(restab$Bestattungen[is.element(restab$Alter,seq(asd$beg[i],asd$ende[i],1))]),y=(asd$cof[i]/(length(seq(asd$beg[i],asd$ende[i],1)))))
+      restab$Deceased[is.element(restab$Age,seq(asd$beg[i],asd$ende[i],1))]=summe(x=(restab$Deceased[is.element(restab$Age,seq(asd$beg[i],asd$ende[i],1))]),y=(asd$cof[i]/(length(seq(asd$beg[i],asd$ende[i],1)))))
     }
   }
 
@@ -133,16 +133,18 @@ prep.life.table=function(x,dec="NA",agebeg,ageend,grnam="NA",methode="NA", age.r
       output1=rbind(output1,as.data.frame(t(colSums(restab[(c(cumsum(meth)[v]+1):cumsum(meth)[u]),],na.rm =TRUE))))
     }
   }
-  # The ages of the "meth" breaks are added as column Alter and the "meth" vector is added as column "a"
-  output1$Alter=cumsum(meth)-meth
+  # The ages of the "meth" breaks are added as column Age and the "meth" vector is added as column "a"
+  output1$Age=cumsum(meth)-meth
   output1$a=meth
 
   # For the output of the function the table is converted to a list containing a dataframe for each group defined in the "Group" variable
-  output=list(test=output1[,c("Alter","a",colnames(output1)[2])])
+  output=list(test=output1[,c("Age","a",colnames(output1)[2])])
   names(output[[1]])[3]="Dx"
+  if((dim(output1)[2]-2)>1){
   for(u in 2:(dim(output1)[2]-2)){
-    output[[u]]=output1[,c("Alter","a",colnames(output1)[u+1])]
+    output[[u]]=output1[,c("Age","a",colnames(output1)[u+1])]
     names(output[[u]])[3]="Dx"
+  }
   }
   names(output)=colnames(output1)[c(-1,-length(colnames(output1)))]
 
