3c20c61d988651eeb0667477e35e583b4908e44a
MartinHinz <martin.hinz@ufg.uni-kiel.de>
2017-06-21 12:51:35 +0200

    added test for warning if input dataset has more than the necessary columns in life.table

--- tests/testthat/test_analytical_functions_api.R
+++ tests/testthat/test_analytical_functions_api.R
@@ -26,8 +26,12 @@ test_that("a mortaar_life_table_list is only produced from a list of data.frames
 
 test_that("a mortaar_life_table_list is only produced from a list of data.frames
           with the numeric columns 'a' and 'Dx'.", {
-  a_wrong_input_dataset <- list(data.frame(a = 1:10, Dx = 1:10), data.frame(a = 1:10))
-  expect_error(life.table(a_wrong_input_dataset),"The data.frames with the following element IDs in the input list don't have the numeric columns 'a' and 'Dx': 2")
-  expect_error(life.table(an_input_dataset), NA)
-})
+            a_wrong_input_dataset <- list(data.frame(a = 1:10, Dx = 1:10), data.frame(a = 1:10))
+            expect_error(life.table(a_wrong_input_dataset),"The data.frames with the following element IDs in the input list don't have the numeric columns 'a' and 'Dx': 2")
+            expect_error(life.table(an_input_dataset), NA)
+          })
 
+test_that("life.table warns if more then the necessary columns are in the input data", {
+  a_wrong_input_dataset <- data.frame(a = 1:10, Dx = 1:10, c= 1:10)
+  expect_warning(life.table(a_wrong_input_dataset), "Note that these additional columns will be dropped in the output.")
+})
