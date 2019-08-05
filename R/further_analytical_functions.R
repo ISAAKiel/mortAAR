d7952cc179c19d19ad3747d34d1d30cbec410a85
Nils Müller-Scheeßel <mueller-scheessel@gmx.net>
2019-08-05 23:33:18 +0200

    A interim update including most of the changes demanded by @nevrome.

--- R/further_analytical_functions.R
+++ R/further_analytical_functions.R
@@ -5,7 +5,8 @@
 #' will necessarily be misleading as they lead to believe that the
 #' mortality of younger children was lower than it actually was and
 #' that life expectancy was higher. For correcting these missing
-#' individuals, Bocquet-Appel and Masset conceived of several
+#' individuals, \emph{Bocquet-Appel and Masset} (1977; see also
+#' \emph{Herrmann et al. 1990}, 307) conceived of several
 #' calculations based on regression analyses of modern comparable
 #' mortality data. However, the applicability of these indices
 #' to archaeological data is highly debated and does not necessarily
@@ -20,15 +21,22 @@
 #' @param agecorfac numeric vector, optional.
 #' @param option_spline integer, optional.
 #'
-#' @return a list containing a data.frame with indices according to
-#' Bocquet-Appel and Masset showing the computed exact value as well
-#' as ranges and an object of class mortaar_life_table with the corrected
-#' values.
+#' @return a list containing a data.frame with indices e0, 1q0 and 5q0
+#' according to Bocquet-Appel and Masset showing the computed exact value
+#' as well as ranges and an object of class mortaar_life_table with the
+#' corrected values.
 #' \itemize{
 #'   \item \bold{e0}:   Corrected life expectancy.
 #'   \item \bold{1q0}:   Mortality of age group 0--1.
 #'   \item \bold{5q0}:   Mortality of age group 0--5.
 #'}
+#'
+#'
+#' @references
+#' \insertRef{masset_bocquet_1977}{mortAAR}
+#'
+#' \insertRef{herrmann_prahistorische_1990}{mortAAR}
+#'
 #'
 #' @examples
 #' # Calculate representativity indices from real life dataset.
@@ -51,16 +59,20 @@ lt.correction <- function(life_table, agecor = TRUE, agecorfac = c(), option_spl
 
   # corrected life expectancy at birth after Bocquet-Appel and Masset
   e0 <- 78.721 * log10(sqrt(1 / indx$juvenile_i$d5_14_d20plus)) - 3.384
-  e0_range <- paste0(round(e0 - 1.503,1), "--", round(e0 + 1.503,1))
+  e0_range_start <- round(e0 - 1.503,1)
+  e0_range_end <- round(e0 + 1.503,1)
 
   # corrected mortality for age group 1q0 after Bocquet-Appel and Masset
   q1_0 <- 0.568 * sqrt((log10(200 * indx$juvenile_i$d5_14_d20plus))) - 0.438
-  q1_0_range <- paste0(round(q1_0 - 0.016,3), "--", round(q1_0 + 0.016,3))
+  q1_0_range_start <- round(q1_0 - 0.016,3)
+  q1_0_range_end <- round(q1_0 + 0.016,3)
 
   # corrected mortality for age group 5q0 after Bocquet-Appel and Masset
   q5_0 <- 1.154 * sqrt((log10(200 * indx$juvenile_i$d5_14_d20plus))) - 1.014
-  q5_0_range <- paste0(round(q5_0 - 0.041,3), "--", round(q5_0 + 0.041,3))
+  q5_0_range_start <- round(q5_0 - 0.041,3)
+  q5_0_range_end <- round(q5_0 + 0.041,3)
 
+  # calculation of life table correction
   Dx_sum_corrected <- (life_table$Dx %>% sum - life_table$Dx[1]) / (1 - q5_0)
   Dx5_0_corrected <- q5_0 * Dx_sum_corrected
 
@@ -76,13 +88,18 @@ lt.correction <- function(life_table, agecor = TRUE, agecorfac = c(), option_spl
          input data should look like for accomplishing this.")
   }
   life_table_prep_corrected <- data.frame(cbind(a = life_table$a, Dx = life_table$Dx))
-  life_table_corr <- life.table(life_table_prep_corrected, agecor = agecor, agecorfac = agecorfac, option_spline = option_spline)
-  row_e0 <- c(round(e0, 1), e0_range)
-  row_q1_0 <- c(round(q1_0, 3), q1_0_range)
-  row_q5_0 <- c(round(q5_0, 3), q5_0_range)
+  life_table_corr <- life.table(life_table_prep_corrected, agecor = agecor,
+                                agecorfac = agecorfac, option_spline = option_spline)
+
+  # putting together the indices data.frame
+  row_e0 <- c(round(e0, 1), e0_range_start, e0_range_end)
+  row_q1_0 <- c(round(q1_0, 3), q1_0_range_start, q1_0_range_end)
+  row_q5_0 <- c(round(q5_0, 3), q5_0_range_start, q5_0_range_end)
   e0_q5_0 <- data.frame(rbind(row_e0, row_q1_0, row_q5_0))
-  colnames(e0_q5_0) <- c("value", "range")
+  colnames(e0_q5_0) <- c("value", "range_start", "range_end")
   rownames(e0_q5_0) <- c("e0", "1q0", "5q0")
+
+  # returning the indices data.frame as well as the corrected life table
   return(list(indices = e0_q5_0, life_table_corr = life_table_corr))
   }
 
@@ -93,23 +110,25 @@ lt.correction <- function(life_table, agecor = TRUE, agecorfac = c(), option_spl
 #' The proportional relation between adult males and females (=
 #' Masculinity index) is interesting for a number of reasons: (1)
 #' it can point to basic problems in the datasets in that, say,
-#' one sex is grossly over- or underrepresented. (2) it may hint
-#' towards cultural reaons like sex-specific mobility.\cr
+#' one sex is grossly over- or underrepresented (\emph{Herrmann et al.
+#' 1990}, 310). (2) it may hint towards cultural reasons like sex-
+#' specific mobility.\cr
 #' Maternal mortality is a basic indicator for the health system of a
-#' given population. Recently, McFadden and Oxenham (2019) have
+#' given population. Maternal mortality is defined as dying during
+#' pregnancy or within the first 42 days after birth due to
+#' complications. Recently, \emph{McFadden and Oxenham 2019} have
 #' provided a formula to calculate it from archaeological data.
 #'
 #' The Masculinity index (MI) is defined for juvenile and older
 #' individuals. Note that with a higher mortality rate of adult females,
 #' an MI < 100 does not necesssarily speak for an unbalanced MI in life.\cr
 #' Maternal mortality is calculated according to the formula
-#' provided by McFadden and Oxenham (2019). McFadden and Oxenham show
+#' provided by \emph{McFadden & Oxenham 2019}. McFadden and Oxenham show
 #' that with modern data a very high correlation is achieved by only
 #' comparing the absolute numbers of the age group 20 to 24. This has the
 #' additional advantage that for this age group anthropological aging
 #' methods are reasonable exact.
 #'
-#' @param life_table an object of class mortaar_life_table_list.
 #' @param females an object of class mortaar_life_table for females
 #' @param males an object of class mortaar_life_table for males
 #'
@@ -123,6 +142,13 @@ lt.correction <- function(life_table, agecor = TRUE, agecorfac = c(), option_spl
 #'
 #'                    \eqn{333.33 * D20-24female / D20-24male - 76.07}
 #'}
+#' @references
+#'
+#' \insertRef{herrmann_prahistorische_1990}{mortAAR}
+#'
+#' \insertRef{mcfadden_oxenham_2019}{mortAAR}
+#'
+#'
 #'
 #' @examples
 #' # Calculate Masculinity index and maternal mortality from real life
@@ -142,41 +168,45 @@ lt.correction <- function(life_table, agecor = TRUE, agecorfac = c(), option_spl
 #' agebeg = "from", ageend = "to", method = "Standard",
 #' agerange = "excluded")
 #' muen_result <- muen_prep %>% life.table()
-#' lt.sexrelation(muen_result,"female","male")
+#' lt.sexrelation(muen_result$female,muen_result$male)
 #'
 #' @export
 #'
-lt.sexrelation <- function(life_table, females, males) {
+lt.sexrelation <- function(females, males) {
 
-  # check if life_table is of class "mortaar_life_table"
-  if (!inherits(life_table,"mortaar_life_table_list"))  {
+  # check if life tables for females and males are of class "mortaar_life_table"
+  if (!inherits(females,"mortaar_life_table") |
+      !inherits(males,"mortaar_life_table"))  {
     paste0(
-      "The dataset does not seem to be a mortAAR life table list."
+      "At least one of the datasets does not seem to be a mortAAR life table."
     ) %>%
       message
   } else {
 
     # Masculinity index (MI) for juvenile and older individuals: males * 100 / females
 
-    fem_age <- life_table[females][[1]][[2]] %>% cumsum
-    fem_sum <- life_table[females][[1]][[4]][which(fem_age >= 20)] %>% sum
-    male_age <- life_table[males][[1]][[2]] %>% cumsum
-    male_sum <- life_table[males][[1]][[4]][which(male_age >= 20)] %>% sum
+    fem_age <- females$a %>% cumsum
+    fem_sum <- females$Dx[which(fem_age >= 20)] %>% sum
+    male_age <- males$a %>% cumsum
+    male_sum <- males$Dx[which(male_age >= 20)] %>% sum
 
     masculinity_index <- round(male_sum * 100 / fem_sum, 1)
 
 
     # compute maternal mortality according to McFaden/Oxenham 2019
-    f_mort <- life_table[females][[1]][[4]][which(life_table[females][[1]][[1]]=="20--24")]
-    m_mort <- life_table[males][[1]][[4]][which(life_table[males][[1]][[1]]=="20--24")]
-    maternal_mortality <- (333.33 * f_mort / m_mort) - 76.07
-    maternal_mortality_rate <- round(100000 / maternal_mortality)
+    f_mort <- females$Dx[which(females$x=="20--24")]
+    m_mort <- males$Dx[which(males$x=="20--24")]
+    f_m_ratio <- f_mort / m_mort
+    maternal_mortality <- 333.33 * f_m_ratio - 76.07
 
-    cat(sep='', "Masculinity index: \n", masculinity_index,
-        "\n\nMaternal mortality:\n", round(maternal_mortality,1),
-        "\n\nOne of ", maternal_mortality_rate, " women is dying during",
-        " pregnancy or within the first 42 days after birth due to",
-        " complications.")
+    # putting together the sex-relation data.frame
+    values <- c(masculinity_index, round(f_m_ratio, 2),
+    round(maternal_mortality,1), round(maternal_mortality / 100,2))
+    description <- c("Masculinity index","Ratio of females to males aged 20--24",
+                     "Maternal mortality per 100,000 births","Maternal mortality per 1,000 births")
+    sexrelation_df <- data.frame(cbind(values, description))
+    row.names(sexrelation_df) <- c("MI","Ratio_F_M","MMR1","MMR2")
+    return(sexrelation_df)
   }
 }
 
@@ -191,35 +221,35 @@ lt.sexrelation <- function(life_table, females, males) {
 #' are unrealistic high or low.
 #'
 #' There are different approaches to calculate reproduction rates (e. g.
-#' Henneberg 1978). We largely follow the methodology by Hassan 1981.
-#' Typically, a Total fertility rate (TFR) of 6-8 is assumed for
-#' prehistoric populations (Ascadi/Nemeskeri 1970; Henneberg 1978; Hassan
-#' 1981). Recently, McFadden and Oxenham (2017) have published a formula
-#' to estimate the Total fertility rate from archaeological data,
-#' provided that infants are represented fully in the archaeological
-#' record.\cr
+#' \emph{Henneberg 1978}). We largely follow the methodology by
+#' \emph{Hassan 1981}. Typically, a Total fertility rate (TFR) of 6-8 is
+#' assumed for prehistoric populations (\emph{Ascadi/Nemeskeri 1970};
+#' \emph{Henneberg 1978}; \emph{Hassan 1981}). Recently, \emph{McFadden
+#' and Oxenham 2018} have published a formula to estimate the Total
+#' fertility rate from archaeological data, provided that infants are
+#' represented fully in the archaeological record.\cr
 #' Unfortunately, this will not be the case for most archaeological
 #' datasets. Therefore, we used the data published by McFadden and
-#' Oxenham to apply it to the P(5-19)-index after Bocquet-Appel. We
-#' approximated the ratio by three different methods of fitting (linear,
-#' logistic, power) and recommend logistic fitting, but the others are
-#' available as well.\cr
+#' Oxenham to apply it to the P(5-19)-index after \emph{Bocquet-Appel
+#' 2002}. We approximated the ratio by three different methods of
+#' fitting (linear, logistic, power) and recommend logistic fitting,
+#' but the others are available as well.\cr
 #' The Gross reproduction rate (GRR) is calculated by multiplying the TFR
 #' with the ratio of female newborns. The Net reproduction rate is
 #' arrived at by summing the product of the GRR, the age specific
-#' fertility rate as defined by Hassan (1981, 137 tab. 8.7) and the age
-#' specific survival taken from the life table and dividing the result
-#' by 10000. The formulas for r and Dt are directly taken from Hassan
-#' (1981, 140).
+#' fertility rate as defined by \emph{Hassan} (1981, 137 tab. 8.7) and
+#' the age specific survival taken from the life table and dividing the
+#' result by 10000. The formulas for r and Dt are directly taken from
+#' \emph{Hassan} (1981, 140).
 #'
 #' @param life_table an object of class mortaar_life_table.
-#' @param fertility_rate string or numeric. Either fertility rate according to McFadden and
-#' Oxenham 2017 if infants are represented well or fertility rate according
-#' to data by McFadden and Oxenham 2017 for P(5-19) index after
-#' Bocquet-Appel. Options: 'McO' (McFadden/Oxenham), 'BA_linear' (linear
-#' fit), 'BA_power' (power fit) or 'BA_log' (logistic fit). Default:
-#' 'BA_log'. Additionally, the user can specify an arbitrary number
-#' in lieu of the fertility rate.
+#' @param fertility_rate string or numeric. Either fertility rate according
+#' to \emph{McFadden & Oxenham 2018} if infants are represented well or
+#' fertility rate according to data by \emph{McFadden & Oxenham 2018} for
+#' P(5-19) index after \emph{Bocquet-Appel 2002}. Options: 'McO'
+#' (McFadden/Oxenham), 'BA_linear' (linear fit), 'BA_power' (power fit)
+#' or 'BA_log' (logistic fit). Default: BA_log'. Additionally, the
+#' user can specify an arbitrary number in lieu of the fertility rate.
 #' @param gen_len numeric. Length of generation for determining
 #' the rate of doubling the population. Default: 20.
 #'
@@ -243,6 +273,18 @@ lt.sexrelation <- function(life_table, females, males) {
 #'
 #'                    \eqn{Dt = 100 * 0.6931 / r}
 #'  }
+#'
+#' @references
+#'
+#' \insertRef{acsadi_history_1970}{mortAAR}
+#'
+#' \insertRef{bocquet_appel_2002}{mortAAR}
+#'
+#' \insertRef{hassan_1981}{mortAAR}
+#'
+#' \insertRef{henneberg_1976}{mortAAR}
+#'
+#' \insertRef{mcfadden_oxenham_2018}{mortAAR}
 #'
 #' @examples
 #' # Calculate reproduction indices from real life dataset.
@@ -283,12 +325,17 @@ lt.reproduction <- function(life_table, fertility_rate = "BA_log",  gen_len = 20
       message
   }
 
+  # Survival rates of individuals aged 15--45.
+  all_age <- life_table$a %>% cumsum
+  fertil_lx <- life_table$lx[which(all_age >15 & all_age <=45)]
+  fertil_lx <- c(fertil_lx,100)
+
   # Gross reproductive rate only for females according to Hassan
   R_pot_fem <- fertil_rate * 0.488
 
   # Net reproduction rate after Hassan
   fertility_perc <- c(10.9, 23.1, 24.1, 19.5, 14.4, 6.2, 1.6)
-  R_0 <- (c(indx$repro_indx$fertil_lx) * c(fertility_perc * R_pot_fem / 10000)) %>% sum
+  R_0 <- (fertil_lx * fertility_perc * R_pot_fem / 10000) %>% sum
 
   # Intrinsic growth rate in percent per year after Hassan
   intr_grow <- 100 * log(R_0) / gen_len
@@ -304,5 +351,5 @@ lt.reproduction <- function(life_table, fertility_rate = "BA_log",  gen_len = 20
   result <- data.frame(rbind(fertiliy_rate, R_pot_fem, R_0, intr_grow, Dt))
   colnames(result) <- c("value", "description")
   rownames(result) <- c("TFR","GRR", "NRR","r", "Dt")
-  format(result, justify = "left")
+  return(result)
 }
--- R/representativity.R
+++ R/representativity.R
@@ -1,10 +1,11 @@
 #' Checks the representativity of the non-adult age groups in a mortAAR
 #' life table
 #'
-#' Weiss 1973 and Bocquet-Appel and Masset 1978 have devised indices
-#' which check if the non-adult age groups are represented in proportions
-#' as can be expected from modern comparable data. Whether this is really
-#' applicable to archaeological data-sets is a matter of debate.
+#' \emph{Weiss 1973} and \emph{Bocquet-Appel and Masset 1977} (see also
+#' \emph{Herrmann et al. 1990, 306f.} have devised indices which check
+#' if the non-adult age groups are represented in proportions as can be
+#' expected from modern comparable data. Whether this is really applicable
+#' to archaeological data-sets is a matter of debate.
 #'
 #' Weiss choose the mortality (qx) as deciding factor and claimed that
 #' (1) the probability of death of the age group 10--15 (5q10)
@@ -20,6 +21,15 @@
 #' @param life_table an object of class mortaar_life_table.
 #'
 #' @return Output showing the indices and explaining their interpretation.
+#'
+#' @references
+#'
+#' \insertRef{herrmann_prahistorische_1990}{mortAAR}
+#'
+#' \insertRef{masset_bocquet_1977}{mortAAR}
+#'
+#' \insertRef{weiss_demography_1973}{mortAAR}
+#'
 #'
 #' @examples
 #' # Calculate representativity indices from real life dataset.
@@ -38,52 +48,53 @@
 #'
 lt.representativity <- function(life_table) {
 
+  mortality <- lt.mortality(life_table)
   indx <- lt.indices(life_table)
 
   # Criteria for representativity after Weiss 1973
 
   representativity_sum <- 0
 
-  weiss_i1 <-  if (indx$mortality$q10_5 < indx$mortality$q15_5) {
+  weiss_i1 <-  if (mortality$q10_5 < mortality$q15_5) {
     representativity_sum <- representativity_sum + 0
-    crayon::green(paste0("5q10 = ", round(indx$mortality$q10_5, 1), " < 5q15 = ",
-                         round(indx$mortality$q15_5, 1),"."))
+    crayon::green(paste0("5q10 = ", round(mortality$q10_5, 1), " < 5q15 = ",
+                         round(mortality$q15_5, 1),"."))
   } else {
     representativity_sum <- representativity_sum + 1
-    crayon::red(paste0("5q10 = ", round(indx$mortality$q10_5, 1), " > 5q15 = ",
-                       round(indx$mortality$q15_5, 1),"."))
+    crayon::red(paste0("5q10 = ", round(mortality$q10_5, 1), " > 5q15 = ",
+                       round(mortality$q15_5, 1),"."))
   }
 
-  weiss_i2 <- if (indx$mortality$q0_5 > indx$mortality$q15_5) {
+  weiss_i2 <- if (mortality$q0_5 > mortality$q15_5) {
     representativity_sum <- representativity_sum + 0
-    crayon::green(paste0("5q0 = ", round(indx$mortality$q0_5, 1), " > 5q15 = ",
-                         round(indx$mortality$q15_5, 1),"."))
+    crayon::green(paste0("5q0 = ", round(mortality$q0_5, 1), " > 5q15 = ",
+                         round(mortality$q15_5, 1),"."))
   } else {
     representativity_sum <- representativity_sum + 1
-    crayon::red(paste0("5q0 = ", round(indx$mortality$q0_5, 1),
-                       " < 5q15 = ", round(indx$mortality$q15_5, 1),"."))
+    crayon::red(paste0("5q0 = ", round(mortality$q0_5, 1),
+                       " < 5q15 = ", round(mortality$q15_5, 1),"."))
   }
 
 
   # Criteria for representativity after Bocquet-Appel and Masset
 
-  child_i <- if (indx$child_i$d5_9_d10_14 < 2) {
+  child_i <- if (indx$child_i < 2) {
     representativity_sum <- representativity_sum + 1
-    crayon::red(paste0("Index of children is ", round(indx$child_i$d5_9_d10_14, 2),
+    crayon::red(paste0("Index of children is ", round(indx$child_i, 2),
                        " and therefore under the expected value of 2."))
   } else {
     representativity_sum <- representativity_sum + 0
-    crayon::green(paste0("Index of children is ", round(indx$child_i$d5_9_d10_14, 2),
+    crayon::green(paste0("Index of children is ", round(indx$child_i, 2),
                          " and therefore above the expected value of 2."))
   }
 
-  juvenile_i <- if (indx$juvenile_i$d5_14_d20plus < 0.1) {
+  juvenile_i <- if (indx$juvenile_i < 0.1) {
     representativity_sum <- representativity_sum + 1
-    crayon::red(paste0("Index of juveniles is ", round(indx$juvenile_i$d5_14_d20plus,2),
+    crayon::red(paste0("Index of juveniles is ", round(indx$juvenile_i,2),
                        " and therefore under the expected value of 0.1."))
   } else {
     representativity_sum <- representativity_sum + 0
-    crayon::green(paste0("Index of juveniles is ", round(indx$juvenile_i$d5_14_d20plus,2),
+    crayon::green(paste0("Index of juveniles is ", round(indx$juvenile_i,2),
                          " and therefore above the expected value of 0.1."))
   }
 
@@ -96,7 +107,7 @@ lt.representativity <- function(life_table) {
   }
 
   cat("Criteria for representativity after Weiss 1973:\n", weiss_i1, "\n", weiss_i2,
-      "\n\nCriteria for representativity after Bocquet-Appel and Masset:\n", child_i, "\n", juvenile_i,
+      "\n\nCriteria for representativity after Masset and Bocquet 1977:\n", child_i, "\n", juvenile_i,
       "\n\nRepresentativity verdict:\n",representativity_verdict)
 
 }
