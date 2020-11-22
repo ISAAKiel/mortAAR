606c6b1eeb0e1cea7892b99a4c21a8a64a28da29
Nils Müller-Scheeßel <mueller-scheessel@gmx.net>
2020-11-22 23:12:45 +0100

    Still no change, therefore replaced nitra-example with magdalenenberg.

--- tests/testthat/test_life_table.R
+++ tests/testthat/test_life_table.R
@@ -3,10 +3,19 @@ test_that("life.table fails for wrong input", {
 })
 
 test_that("life.table produces the right output with spline-function", {
-  nitra_prep <- prep.life.table(nitra, method="Equal5",
-                                agebeg = "age_start", ageend = "age_end")
+  magdalenenberg <- magdalenenberg %>% replace(magdalenenberg == "60-x", "60-69")
+  magdalenenberg <- magdalenenberg %>% tidyr::separate(a, c("from", "to")) %>%
+    transform(from = as.numeric(from), to = as.numeric(to))
+  mag_prep <- magdalenenberg %>%
+    prep.life.table(
+      dec = "Dx",
+      agebeg = "from",
+      ageend = "to",
+      method = "Equal5",
+      agerange = "included"
+    )
   expect_snapshot_value(
-    life.table(nitra_prep,option_spline = 10),
+    life.table(mag_prep,option_spline = 10),
     style = c("serialize")
     # style needs to be 'serialize' (and thus completely opaque) because
     # there seem to be rounding errors with 'json2'
